9c10880792c100d2d3a53283fcb3e4adbbfb1d2d
aruna <arunakamakshi09@gmail.com>
2025-03-13 11:21:25 +0530

    Added the Link & Script component.

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import WithCacheComponent from "./components/WithCacheComponent";
 import Counter from "@/app/components/Counter";
 import ClientComponent from "@/app/components/ClientComponent";
 import Image from "next/image";
+import Link from "next/link";
+import Script from "next/script";
 
 export default function Home() {
     return (
@@ -29,6 +31,17 @@ export default function Home() {
                     marginLeft: 'auto',
                     marginRight: 'auto',
                 }}
+            /><br/>
+
+            <h1>Next.js 15 - Fast Navigation</h1>
+            <Link href="/about">
+                <button>Go to About Page</button>
+            </Link><br/>
+
+            <h1>Next.js 15 - Third-Party Script</h1>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=GA_TRACKING_ID"
+                strategy="afterInteractive" // Loads script after page loads
             />
         </div>
     );
